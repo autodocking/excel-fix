43111d4ccdc2e8ff4990cbea4f8049bce66e2451
autodocking <autodocking@outlook.com>
2020-12-30 18:00:12 +0800

    12.30 使用 vue 重建

--- rebuild/README.md
+++ rebuild/README.md
@@ -1,10 +1,8 @@
 # excel-fix
 
-Under building, not available yet.
-
 **待办事项**
 
-- [ ] 使用 bootstrapVue 重建已有功能
+- [x] 使用 bootstrapVue 重建已有功能
 - [ ] 对 styles.xml 以外的文件进行处理
 - [ ] 页面中增加选项，让用户可选择性地进行处理
 - [ ] png 格式批量转化为 jpg 格式
--- rebuild/main.js
+++ rebuild/main.js
@@ -3,14 +3,13 @@
 var app = new Vue({
     el: '#app',
     data: {
-        alert: false,
-        alertType: 'danger',
-        alertMsg: '读取错误',
         names: [],
         isDragIn: false,
-        startTime: '', timecounts: [],
-        logsHeader: '未选择任何文件',
-        logs: []
+        startTime: null,
+        logs: {},
+        step: 0,
+        totalStep: 5,
+        showMultipleFileWarning: false
     },
     methods: {
         onDragenter: function (e) {
@@ -20,125 +19,99 @@ var app = new Vue({
             this.isDragIn = false;
         },
         onGetFiles: function (e) {
-            // 记录开始时间
-            this.startTime = new Date().getTime();
-            this.logs = [];
-            this.logsHeader = getHHMMSS() + ' 开始处理文件';
+            // 判断是选择还是拖入的
             if (e.type == 'drop') {
                 var files = e.dataTransfer.files;
                 this.isDragIn = false;
             } else {
                 var files = e.target.files;
             }
+            // 初始化
+            this.startTime = new Date().getTime();
+            this.logs = {};
+            this.alerts = {};
+            this.step = 0;
+            this.totalStep = (files.length) * 5;
+            if (files.length > 1) {
+                this.showMultipleFileWarning = true;
+            } else {
+                this.showMultipleFileWarning = false;
+            }
+            // 所有文件并行处理
             for (var i = 0; i < files.length; i++) {
-                console.log(this)
-                handleFile(files[i], i + 1);
+                handleFile(files[i], i);
             }
         },
-
-        handleFile: function (e) {
-
-            console.log(this)
-
-
-            // 为每一个文件独立计时
-            var pid = Math.random();
-            timecounts[pid] = {}
-            timecounts[pid].dateBefore = new Date();
-
-            var $title = $('<h4>', {
-                text: f.name
-            });
-            var $fileContent = $('<ul>');
-            $result.append($title);
-            $result.append($fileContent);
-
-            JSZip.loadAsync(f)
-                .then(function (zip) {
-                    log('已完成解压，请耐心等候...');
-                    if (!(zip.file('xl/styles.xml'))) {
-                        log('找不到【 ./xl/styles.xml 】文件。', 1)
-                        $result.append($('<div>', {
-                            'class': 'alert alert-danger',
-                            text: '读取错误：请确认文件【 ' + f.name + ' 】是否为xlsx格式，如果是旧版本的xls文件，请先在Excel中另存为xlsx格式。'
-                        }));
-                        return;
-                    }
-                    zip.file('xl/styles.xml').async('string').then(function (temp) {
-                        // 清除自定义样式
-                        temp = temp.replace(/<cellStyles.*<\/cellStyles>/, '')
-                        log('已清除自定义样式。');
-                        // 写入zip
-                        zip.file('xl/styles.xml', temp);
-                        // 输出zip到FileSaver.min.js
-                        log('正在合成文件...', 1);
-                        zip.generateAsync({
-                            type: 'blob',
-                            compression: 'DEFLATE',
-                            compressionOptions: { level: 6 }
-                        })
-                            .then(function (blob) {
-                                log('已完成！请留意下载内容。');
-                                // 开始下载
-                                saveAs(blob, f.name);
-                                log('请在Excel中打开并重新保存一次该文件，将进一步缩小体积。', 1);
-                            });
-                    });
-                }, function (e) {
-                    $result.append($('<div>', {
-                        'class': 'alert alert-danger',
-                        text: '读取错误：请确认文件【 ' + f.name + ' 】是否为xlsx格式，如果是旧版本的xls文件，请先在Excel中另存为xlsx格式。'
-                    }));
-                    console.log(f.name + ': ' + e.message);
-                });
-
-            function log(text, a) {
-                if (a == undefined) {
-                    $fileContent.append($("<li>", {
-                        text: text + '(' + timePast() + 'ms)'
-                    }));
-                } else {
-                    $fileContent.append($("<li>", {
-                        text: text
-                    }));
-                }
+        log: function (key, value, variant) {
+            if (!this.logs[key]) {
+                this.logs[key] = { step: 0 }
             }
-
-            function timePast() {
-                timecounts[pid].dateAfter = new Date();
-                var timePast = timecounts[pid].dateAfter - timecounts[pid].dateBefore;
-                timecounts[pid].dateBefore = timecounts[pid].dateAfter;
-                return timePast;
-            }
-
-
-        }
+            var timePast = '耗时 ' + (((new Date).getTime() - this.startTime) / 1000).toFixed(1) + 's';
+            this.logs[key].timePast = timePast;
+            this.logs[key].value = value;
+            this.logs[key].variant = variant;
+            // 由于vue限制，对 logs 深层数值的多次更新，视图只显示初始化之后的第一次更新
+            // 因此通过更新处于数据最外层的进度条 step，使深层日志的每次更新也都能被立即显示
+            this.step += 1
+        },
 
     }
 })
 
-
+// 处理文件
 function handleFile(f, i) {
-    if (!app.logs[i]) {
-        app.logs[i] = []
-    }
-    app.logs[i].push(getHHMMSS() + ' 读取' + f.name);
-    app.logs[i].push(getHHMMSS() + ' step 1');
 
-    setTimeout(() => {
-        console.log(app)
-        app.logs[i].push(getHHMMSS() + ' step 2');
-        
-    }, 1000);
+    app.log(f.name, '读取文件。')
+
+    JSZip.loadAsync(f)
+        .then(function (zip) {
+
+            app.log(f.name, '解压文件。')
+
+            if (!(zip.file('xl/styles.xml'))) {
+                var msg = '文件已解压，但是未找到 /xl/styles.xml，这可能不是一个 Excel 文件。'
+                // app.log(f.name, msg)
+                app.log(f.name, msg, 'text-danger');
+                app.step += 2;
+                return;
+            }
+            zip.file('xl/styles.xml').async('string').then(function (temp) {
+                // 清除自定义样式
+                app.log(f.name, '清除自定义样式。')
+                temp = temp.replace(/<numFmts.*<\/numFmts>/, '');
+                temp = temp.replace(/<cellStyleXfs.*<\/cellStyleXfs>/, '');
+                temp = temp.replace(/<cellStyles.*<\/cellStyles>/, '');
+                // 修改后的文件写入zip
+                zip.file('xl/styles.xml', temp);
+
+                app.log(f.name, '重新压缩文件。');
+                zip.generateAsync({
+                    type: 'blob',
+                    compression: 'DEFLATE',
+                    compressionOptions: { level: 6 }
+                }).then(function (blob) {
+                    // 输出到FileSaver.min.js
+                    saveAs(blob, f.name);
+                    app.log(f.name, '√ 完成！', 'text-success');
+                });
+            });
+        }, function (e) {
+            var msg = '解压错误，请确认文件是 xlsx 格式，如果是旧版本的 xls 文件，请先在 Excel 中另存为 xlsx 格式。';
+            app.log(f.name, msg, 'text-danger')
+            app.step += 3;
+        });
+
 }
 
-function double(a) {
-    return Number(a) < 10 ? ('0' + String(a)) : a
-}
 
+// 获取 00:00:00 格式时间
 function getHHMMSS(t) {
     if (!t) {
         t = new Date()
     }
-    return double(t.getHours()) + ':' + double(t.getMinutes()) + ':' + double(t.getSeconds())
+    return double(t.getHours()) + ':' + double(t.getMinutes()) + ':' + double(t.getSeconds()) + ':' + (t.getMilliseconds())
+
+    function double(a) {
+        return Number(a) < 10 ? ('0' + String(a)) : a
+    }
 }
